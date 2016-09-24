b3006dcf946376b12f30f18200f3f09e621702df
Chris Roth <christieroth21@gmail.com>
2016-09-24 15:28:21 -0400

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float maxSpeed = 3;
+    public float speed = 50f;
+    public float jumpPower = 500f;
+
+    public bool grounded;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+	private Camera camera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+		anim = gameObject.GetComponent<Animator>();
+		camera = this.GetComponentInChildren<Camera>() ;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        anim.SetFloat("speed", Mathf.Abs(rb2d.velocity.x));
+        anim.SetBool("grounded", grounded);
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(-8, 8, 1);
+        }
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(8, 8, 1);
+        }
+  
+        if(Input.GetButtonDown("Jump") && grounded == true) 
+        {
+            rb2d.AddForce(Vector2.up * jumpPower);
+        }
+		float h = Input.GetAxis ("Horizontal");
+		//if( Input.GetMouseButton(0) )
+			//rb2d.AddForce(Vector2.up * jumpPower);
+
+
+		if (Input.GetMouseButton(0)) {
+			
+			//Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			//Camera camera = GetComponent<GameObject.Camera>();
+			Vector2 p = camera.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+			rb2d.AddForce ((p * speed));
+		}
+
+	}
+    void FixedUpdate()
+	{
+		Vector3 easeVelocity = rb2d.velocity;
+		easeVelocity.y = rb2d.velocity.y;
+		easeVelocity.z = 0f;
+		easeVelocity.x *= .75f;
+
+		// fake friction / easing the x speed of player
+
+		if (grounded) {
+			rb2d.velocity = easeVelocity;
+		}
+
+		float h = Input.GetAxis ("Horizontal");
+		//h = speed of player -1 or 1 depending on direction
+		//moving player
+		rb2d.AddForce ((Vector2.right * speed) * h);
+		//limits max speed
+		if (rb2d.velocity.x > maxSpeed) {
+			rb2d.velocity = new Vector2 (maxSpeed, rb2d.velocity.y);
+		}
+		if (rb2d.velocity.x < -maxSpeed) {
+			rb2d.velocity = new Vector2 (-maxSpeed, rb2d.velocity.y);
+		}
+
+	}
+
+}
