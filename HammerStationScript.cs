ad4f48775862b488d338623d32fce83281c2977d
CharliePi <Charliep92@hotmail.com>
2016-10-30 18:57:46 -0400

    script to make the hammer time work

---
+++ HammerStationScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HammerStationScript : MonoBehaviour {
+
+    private HammerScript hammer;
+
+
+    void Start()
+    {
+        hammer = gameObject.GetComponentInParent<HammerScript>();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "hammerplatform")
+        {
+            hammer.grounded = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "hammerplatform")
+        {
+            hammer.grounded = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "hammerplatform")
+        {
+            hammer.grounded = false;
+        }
+    }
+}
